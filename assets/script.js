fd87f903716d9a9493e52696adda5e71daf2a7cf
BryceedThompson <128741755+BryceedThompson@users.noreply.github.com>
2023-05-08 18:05:00 -0700

    html setup and todos

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+// build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+// Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={27a78ec5884e4d224e3f074788088534}
+
+// use localStorage to store any persistent data. For more information
+
+// WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+// WHEN I view current weather conditions for that city THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+
+// WHEN I view future weather conditions for that city THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// WHEN I click on a city in the search history THEN I am again presented with current and future conditions for that cit
+
+// TODO: function that will take input of city and get the lat and lon cords for other function
+
+// TODO: function that uses lat and lon cords to get data that we want from openweather such as temp, wind, humitity, for the next 5 days
+
+// TODO: function that will recive weatherdata from other function and render it on the page and make html elements for them
+
+// TODO: function that will render seach history and create button to look up past seaches 
+
+
+// TODO: function will retrive search history from local storage and render it on the page
